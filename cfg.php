c33d3d7ff5dcdc0e665dd27fb91f32984777caa2
martinuspatandianan1 <63387920+martinuspatandianan1@users.noreply.github.com>
2020-04-29 15:48:49 +0000

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,16 @@
+<?php
+
+//masukan sesuai dibawah
+$cookie4 = "cookie: __cfduid=dd4d46cad8b3a1ece784ebd8f6cbb20491585743107";
+$cookie5 = "cookie: ExpressCrypto=bfdaf08b0e65b3d9f3a4abe8149a82f0";
+$cookie6 = "cookie: _ga=GA1.1.666361054.1585743119";
+$cookie8 = "cookie: __gads=ID=8640f7647eb588ee:T=1587317307:S=ALNI_MaSgdDWsqu2BmPvliZJGbhclL6uhQ";
+$cookie9 = "cookie: _ga_8ES1N2JB0Q=GS1.1.1588014569.18.1.1588016158.0";
+$clientseedbtc = "pgzij1shrvwyxbgvbmt";
+$clientseedltc = "hvcr2od5nbk3gfc7rkee6";
+$clientseedoge = "f5lhuueva4n6f2g0v";
+
+
+?>
+
+
