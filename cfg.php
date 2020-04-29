fafc719e0a38738731c91b9501e243edb815fc09
martinuspatandianan1 <63387920+martinuspatandianan1@users.noreply.github.com>
2020-04-30 00:55:24 +0800

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,16 +1,14 @@
 <?php
 
 //masukan sesuai dibawah
-$cookie4 = "cookie: __cfduid=dd4d46cad8b3a1ece784ebd8f6cbb20491585743107";
-$cookie5 = "cookie: ExpressCrypto=bfdaf08b0e65b3d9f3a4abe8149a82f0";
-$cookie6 = "cookie: _ga=GA1.1.666361054.1585743119";
-$cookie8 = "cookie: __gads=ID=8640f7647eb588ee:T=1587317307:S=ALNI_MaSgdDWsqu2BmPvliZJGbhclL6uhQ";
-$cookie9 = "cookie: _ga_8ES1N2JB0Q=GS1.1.1588014569.18.1.1588016158.0";
+$cookie4 = "cookie: __cfduid=dd3652b73615216a6cc1ad148c8babc711587925821";
+$cookie5 = "cookie: ExpressCrypto=df97381f622f6905ec70fbf2870a0a23";
+$cookie6 = "cookie: _ga=GA1.1.125957910.1587925867";
+$cookie8 = "cookie: __gads=ID=e495a0948fce577b:T=1587925941:S=ALNI_MY9uysjy82_F5TN8CJwYBJxTdnllQ";
+$cookie9 = "cookie: _ga_8ES1N2JB0Q=GS1.1.1588176613.21.1.1588176620.0";
 $clientseedbtc = "pgzij1shrvwyxbgvbmt";
 $clientseedltc = "hvcr2od5nbk3gfc7rkee6";
-$clientseedoge = "f5lhuueva4n6f2g0v";
+$clientseedoge = "gx7z5no2ltbi";
 
 
 ?>
-
-
